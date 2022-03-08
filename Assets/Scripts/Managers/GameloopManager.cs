177793f483754107e50aacdd117e63d9dabab9a1
WilliamAB <liams_1998@hotmail.com>
2022-03-09 01:29:17 +0200

    Create GameManager + GameLoopManager

---
+++ Assets/Scripts/Managers/GameloopManager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utils.GenericSingletons;
+
+public class GameloopManager : MonoBehaviourSingleton<GameloopManager>
+{
+
+}
